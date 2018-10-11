0f11f164d979d2d82e74cfcc48ad782db606305e
DESKTOP-47R46TC\EvMachine <murad1985@gmail.com>
2018-10-11 03:13:48 -0500

    Creat Throttle

---
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track in Neutons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40ton tank and 1g acceleration
+};
